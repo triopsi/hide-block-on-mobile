a4d37f700118bfac8935183471bf1bd8be654df1
Triopsi <daniel@triopsi.com>
2026-02-11 11:02:19 +0000

    Refactor styles and remove unnecessary files
    - Deleted source map files for index.js and style-index.css to clean up the build.
    - Minified CSS for both style-index.css and style-index-rtl.css to improve loading performance.
    - Adjusted SCSS formatting for better readability in style.scss.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '04f10e037af712bbb198');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '846b8a6d5936eeb5f481');
